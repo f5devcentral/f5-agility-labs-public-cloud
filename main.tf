3a6a1d7f21d8ddb84ea02d3192727f35c9309279
Tony Marfil <tonymarfil@gmail.com>
2017-06-02 14:30:20 -0400

    Deployed new security group, subnets and ami all to new vpc successfully

--- main.tf
+++ main.tf
@@ -6,6 +6,62 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "terraform-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink   = "false"
+
+  tags {
+    Name = "terraform"
+  }
+}
+
+resource "aws_subnet" "public-1" {
+  vpc_id                  = "${aws_vpc.terraform-vpc.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-1d"
+
+  tags {
+    Name = "public"
+  }
+}
+
+resource "aws_subnet" "private-1" {
+  vpc_id                  = "${aws_vpc.terraform-vpc.id}"
+  cidr_block              = "10.0.100.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "us-east-1d"
+
+  tags {
+    Name = "private"
+  }
+}
+
+resource "aws_subnet" "public-2" {
+  vpc_id                  = "${aws_vpc.terraform-vpc.id}"
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-1e"
+
+  tags {
+    Name = "public"
+  }
+}
+
+resource "aws_subnet" "private-2" {
+  vpc_id                  = "${aws_vpc.terraform-vpc.id}"
+  cidr_block              = "10.0.200.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "us-east-1e"
+
+  tags {
+    Name = "private"
+  }
+}
+
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-40d28157"
   instance_type   = "t2.micro"
@@ -17,6 +73,51 @@ resource "aws_launch_configuration" "example" {
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_instance" "deployedToNewVPC" {
+  ami             = "ami-40d28157"
+  instance_type   = "t2.micro"
+  key_name        = "${var.key_pair}"
+  security_groups = ["${aws_security_group.testsg.id}"]
+  subnet_id       = "${aws_subnet.public-1.id}"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  tags {
+    Name = "deployedToNewVC"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "testsg" {
+  name   = "testsg"
+  vpc_id = "${aws_vpc.terraform-vpc.id}"
+
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   lifecycle {
     create_before_destroy = true
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,23 @@
 output "elb_dns_name" {
   value = "${aws_elb.example.dns_name}"
 }
+
+output "vpc-id" {
+  value = "${aws_vpc.terraform-vpc.id}"
+}
+
+output "vpc-publicsubnet" {
+  value = "${aws_subnet.public-1.cidr_block}"
+}
+
+output "vpc-publicsubnet-id" {
+  value = "${aws_subnet.public-1.id}"
+}
+
+output "vpc-privatesubnet" {
+  value = "${aws_subnet.private-1.cidr_block}"
+}
+
+output "vpc-privatesubnet-id" {
+  value = "${aws_subnet.private-1.id}"
+}
