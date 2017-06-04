b331360d258c7824346d0288a860dfbc5528b1b7
Tony Marfil <tonymarfil@gmail.com>
2017-06-04 14:38:27 -0400

    everything works before attempt to integrate f5 cloudformation template

--- vars.tf
+++ vars.tf
@@ -5,18 +5,15 @@ variable "server_port" {
 
 variable "key_pair" {
   description = "The name of an existing key pair. In AWS Console: NETWORK & SECURITY -> Key Pairs"
-  type        = "string"
   default     = "MyKeyPair"
 }
 
 variable "licenseKey1" {
   description = "license key 1"
-  type        = "string"
   default     = "11111-11111-11111-11111-11111-11111"
 }
 
 variable "licenseKey2" {
   description = "license key 2"
-  type        = "string"
   default     = "22222-22222-22222-22222-22222-22222"
 }
